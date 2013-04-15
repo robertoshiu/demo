79282e86967f38c2e3cba08dcfce22b1ff77cf03
Roberto <roberto.hsu@gmail.com>
2013-04-15 23:21:37 +0800

    flow integration

--- SWF-Demo/src/main/java/com/chainmeans/poc/HomeController.java
+++ SWF-Demo/src/main/java/com/chainmeans/poc/HomeController.java
@@ -1,7 +1,5 @@
 package com.chainmeans.poc;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -10,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
 
 /**
  * Handles requests for the application home page.
@@ -22,15 +22,13 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "home.htm", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+				
+		String formattedDate = DateTimeFormat.forPattern("yyyy-MM-dd kk-mm-ss").print(DateTime.now());
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
+		logger.info("The Server Time is {}.", formattedDate);
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
